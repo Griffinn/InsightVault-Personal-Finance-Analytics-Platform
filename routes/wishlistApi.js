4eab8ca096d3643305df2028e3a4d670a47b0819
Khushi Prasad <119476393+Griffinn@users.noreply.github.com>
2025-11-23 15:01:00 +0530

    Delete routes directory

--- routes/wishlistApi.js
+++
@@ -1,9 +0,0 @@
-// wishlistApi.js
-import express from "express";
-import { getWishlist } from "../controllers/wishlistController.js";
-
-export const wishlistApiRouter = express.Router();
-
-wishlistApiRouter.get("/", getWishlist);
-
-
